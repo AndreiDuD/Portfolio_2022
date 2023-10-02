3d2c7005834970b77e7fc24451e93d324a0ae4b2
AndreiDuD <andrei.duduman123@gmail.com>
2023-10-02 14:00:30 +0300

    re-do portfolio

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-400 bg-red-600">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
